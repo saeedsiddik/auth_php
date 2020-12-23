d9a3563469dcc3ebd263343ea050e5f3ccaa611b
Saeed Siddik <saeed.siddik@iit.du.ac.bd>
2020-12-23 21:25:16 +0600

    Add files for register user
    Add 2 files for user registration

---
+++ check_register.php
@@ -0,0 +1,50 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "test";
+	$tbl_name="user"; // Table name 
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+		echo "Database Connection failed ";
+        echo '<a href="register.php"><button>back</button></a>';
+	}
+
+
+	// username and password sent from form 
+	$myusername=$_POST['myusername']; 
+	$mypassword=$_POST['mypassword']; 
+    $my_fullname=$_POST['my_fullname']; 
+
+	// To protect MySQL injection (more detail about MySQL injection)
+	$myusername = stripslashes($myusername);
+	$mypassword = stripslashes($mypassword);
+    $my_fullname = stripslashes($my_fullname);
+
+	$myusername = mysqli_real_escape_string($conn, $myusername);
+	$mypassword = mysqli_real_escape_string($conn, $mypassword);
+	$my_fullname = mysqli_real_escape_string($conn, $my_fullname);
+
+    $my_password_hash = hash('sha512', $mypassword);
+
+	$sql="SELECT * FROM $tbl_name WHERE BINARY username='$myusername' and password='$my_password_hash'";
+
+	$sql = 
+        "INSERT INTO $tbl_name (username, password, full_name)
+	   VALUES 
+    ('$myusername', '$my_password_hash', '$my_fullname')";
+	
+    
+    if (mysqli_query($conn, $sql)) {
+		echo "Register successfully";
+        echo '<a href="main_login.php"><button>Login</button></a>';
+	} else {
+	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+
+?>
+
---
+++ register.php
@@ -0,0 +1,34 @@
+<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+    <tr>
+    <form name="form1" method="post" action="check_register.php">
+    <td>
+    <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+    <tr>
+    <td colspan="3"><strong>Member Register </strong></td>
+    </tr>
+    <tr>
+    <td width="78">Full Name</td>
+    <td width="6">:</td>
+    <td width="294"><input name="my_fullname" type="text" id="my_fullname"></td>
+    </tr>
+    <tr>
+    <td width="78">Username</td>
+    <td width="6">:</td>
+    <td width="294"><input name="myusername" type="text" id="myusername"></td>
+    </tr>
+    <tr>
+    <td>Password</td>
+    <td>:</td>
+    <td><input name="mypassword" type="password" id="mypassword"></td>
+    </tr>
+    <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td><input type="submit" name="Submit" value="Register"></td>
+    </tr>
+    </table>
+    </td>
+    </form>
+    </tr>
+</table>
+
